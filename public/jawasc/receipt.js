6a35612d61aab64b14ab962da9848a24835ba47e
KepoDes <86428587+Kepoo311@users.noreply.github.com>
2024-04-27 23:05:59 +0700

    Some change

--- public/jawasc/receipt.js
+++ public/jawasc/receipt.js
@@ -1,26 +1,31 @@
 const items = document.querySelectorAll('#item');
-const receipt = document.getElementById('receipt');
+        const receipt = document.getElementById('receipt');
 
-items.forEach((item) => {
-   item.addEventListener('click', () => {
-        const proCard = document.getElementById('proCard').dataset.imgName;
-        const item_name = item.querySelector('#item_name').textContent;
-        const item_price = item.querySelector('#item_price').textContent;
+        items.forEach((item) => {
+            item.addEventListener('click', () => {
+                const proCard = document.getElementById('proCard').getAttribute('src');
+                const item_name = item.querySelector('#item_name').textContent;
+                const item_price = item.querySelector('#item_price').textContent;
 
-        const data = showReceipt(proCard,item_name, item_price);
+                const data = showReceipt(proCard, item_name, item_price);
 
-        receipt.classList.remove('hidden');
-        receipt.classList.add('flex');
-        receipt.innerHTML = data;
-   });
-});
+                if (receipt.classList.contains('hidden')) {
+                    receipt.classList.remove('hidden');
+                    receipt.classList.add('flex');
+                }
 
-function showReceipt(card,name,price) {
-    return `
-    <img id="receipt_img" class="h-28 w-24 m-3" src="{{ asset('proCard/${card}') }}" alt="GAS">
+                receipt.classList.remove('hidden');
+                receipt.classList.add('flex');
+                receipt.innerHTML = data;
+            });
+        });
+
+        function showReceipt(card, name, price) {
+            return `
+    <img id="receipt_img" class="h-28 w-24 m-3" src="${card}" alt="GAS">
                              <div class="flex flex-col mt-2">
                                 <h1 id="receipt_packet" class="text-white font-monts font-semibold">${name}</h1>
                                 <h1 id="receipt_price" class="text-white font-monts font-light">${price}</h1>
                              </div>
     `
-}
+        }
